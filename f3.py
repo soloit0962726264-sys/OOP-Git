4ba20a04e3ad7fbc52e9811c023b58afc119a288
Sitthiphong <Sitthiphong.in@live.rmutl.ac.th>
2026-01-06 15:26:25 +0700

    แก้บรรทัดที่ 13

--- f3.py
+++ f3.py
@@ -1,5 +1,5 @@
 width = int(input('กว้าง'))
-width = int(input('ยาว'))
+hight = int(input('ยาว'))
 
 def area (width, hight):
     reg = width * hight
@@ -7,6 +7,6 @@ def area (width, hight):
     print(f'reg = {reg} and tri = {tri}')
     return (reg,tri)
 
-area_reg, area_tri = area(width, hight)
+area_reg, area_tri = area (width, hight)
 print("พื้นที่สี่เหลี่ยม: {area_reg}")
 print(f"พื้นที่สามเหลี่ยม: {area_tri}")
